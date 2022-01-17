ba902e5b503135f5c7c9d71b30052c39f8c7e83c
Houssam El Affas <houssam@MacBook-Air-de-Houssam.local>
2022-01-17 22:13:00 +0100

    feat: add login page and use Store react redux and implement charte colors

---
+++ mobile/charte/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    primary: "#060887", //bleu foncé
+    secondary: "#91CDFF", //bleu clair
+    txt_create_account: "#2F88FA",
+    login: {
+        txt_input: "#B1B3B4",
+        backg_input: "#F4F6F8",
+        btn_login: "#2F88FA",    
+    },
+}
+
+export default colors
