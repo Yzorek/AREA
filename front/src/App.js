d2e07d252a26ea560093ce18f4a3e92056e1c798
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-01-22 18:04:46 +0100

    fix(front): theme active

--- front/src/App.js
+++ front/src/App.js
@@ -8,16 +8,14 @@ import Register from "./Components/Register/Register";
 import Main from "./Components/Main/Main";
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {createTheme, ThemeProvider} from "@mui/material";
-import {theme_default} from "./Components/Main/config";
 
 function App() {
     const [isConnected, setIsConnected] = useState(false);
     let navigate = useNavigate();
 
-    const theme = createTheme(theme_default);
-
     useEffect(() => {
+        if (isConnected)
+            return;
         (async () => {
             const response = await axios.get(`${process.env.REACT_APP_DASHBOARD_API}/auth`,
                 {'headers': {'Authorization': `Bearer  ${localStorage.getItem('JWT')}`}})
@@ -31,22 +29,20 @@ function App() {
     }, [navigate])
 
     return (
-        <ThemeProvider theme={theme}>
-            <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="Login" element={<Login/>}/>
-                <Route path="Register" element={<Register/>}/>
-                <Route path="App/*" element={<Main/>}/>
-                <Route
-                    path="*"
-                    element={
-                        <main style={{ padding: "1rem" }}>
-                            <p>There's nothing here!</p>
-                        </main>
-                    }
-                />
-            </Routes>
-        </ThemeProvider>
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="Login" element={<Login/>}/>
+            <Route path="Register" element={<Register/>}/>
+            <Route path="App/*" element={<Main/>}/>
+            <Route
+                path="*"
+                element={
+                    <main style={{padding: "1rem"}}>
+                        <p>There's nothing here!</p>
+                    </main>
+                }
+            />
+        </Routes>
     );
 }
 
--- front/src/Components/Main/Main.js
+++ front/src/Components/Main/Main.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import {Box, Grid} from "@mui/material";
+import {Box, createTheme, Grid, ThemeProvider} from "@mui/material";
 import AppBarArea from "./AppBarArea";
 import DrawerArea from "./DrawerArea";
 import {Navigate, Route, Routes} from "react-router-dom";
 import Profile from "../Profile/Profile";
+import {theme_default} from "./config";
+
 
 export default function Main() {
-    return <Grid container item xs={12}>
-        <AppBarArea/>
-        <DrawerArea/>
-        <Box component="main" sx={{flexGrow: 1, overflow: 'auto', bgcolor: 'grey.100', height: 'calc(100vh - 68px)'}}>
-            <Routes>
-                <Route path={`/`} element={<Navigate to={'Dashboard'}/>}/>
-                <Route path={`Dashboard`} element={<div>Dashboard</div>}/>
-                <Route path={`Profile/*`} element={<Profile/>}/>
-                <Route
-                    path="*"
-                    element={
-                        <main style={{ padding: "1rem" }}>
-                            <p>There's nothing here!</p>
-                        </main>
-                    }
-                />
-            </Routes>
-        </Box>
-    </Grid>
+    const theme = createTheme(theme_default);
+
+    return <ThemeProvider theme={theme}>
+        <Grid container item xs={12}>
+            <AppBarArea/>
+            <DrawerArea/>
+            <Box component="main"
+                 sx={{flexGrow: 1, overflow: 'auto', bgcolor: 'grey.100', height: 'calc(100vh - 68px)'}}>
+                <Routes>
+                    <Route path={`/`} element={<Navigate to={'Dashboard'}/>}/>
+                    <Route path={`Dashboard`} element={<div>Dashboard</div>}/>
+                    <Route path={`Profile/*`} element={<Profile/>}/>
+                    <Route
+                        path="*"
+                        element={
+                            <main style={{padding: "1rem"}}>
+                                <p>There's nothing here!</p>
+                            </main>
+                        }
+                    />
+                </Routes>
+            </Box>
+        </Grid>
+    </ThemeProvider>
 }
