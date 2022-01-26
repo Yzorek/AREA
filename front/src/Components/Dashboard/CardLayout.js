1d8314fe63dd125bdc5c837ef183b2727c8f76a9
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-01-26 22:17:10 +0100

    feat(front): widget settings and archi widget management

--- front/src/Components/Dashboard/CardLayout.js
+++ front/src/Components/Dashboard/CardLayout.js
@@ -3,9 +3,8 @@ import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import { withSize } from "react-sizeme";
-import {Grid, IconButton, Paper, Tooltip} from "@mui/material";
-import {Cancel, Settings} from "@mui/icons-material";
-import WidgetAction from "./Widget/WidgetAction";
+import {Grid} from "@mui/material";
+import Widget from "./Widget/Widget";
 
 function getFromLS(key) {
     let ls = {};
@@ -44,11 +43,9 @@ function CardLayout({size, isEdit}) {
         >
             <div
                 key={`1 - 1`}
-                data-grid={{w: 5, h: 5, x: 0, y: Infinity}}
+                data-grid={{w: 3, h: 3, x: 0, y: Infinity}}
             >
-                <Paper style={{height: '100%', width: "100%"}}>
-                    {isEdit && <WidgetAction handleRemoveItem={handleRemoveItem} handleOpenSettings={() => {}} />}
-                </Paper>
+                <Widget isEdit={isEdit} handleRemoveItem={handleRemoveItem} idWidget={1}/>
             </div>
         </ResponsiveGridLayout>
     </Grid>
--- front/src/Components/Dashboard/Widget/WidgetAction.js
+++ front/src/Components/Dashboard/Widget/WidgetAction.js
@@ -3,15 +3,15 @@ import {Grid, IconButton, Tooltip} from "@mui/material";
 import {Cancel, Settings} from "@mui/icons-material";
 
 export default function WidgetAction({handleRemoveItem, handleOpenSettings}) {
-    return <Grid container item xs={12} justifyContent={'flex-end'} style={{height: 0}}>
-        <Grid item style={{position: 'relative', left: 20, top: -20}}>
+    return <Grid container item xs={12} justifyContent={'flex-end'} style={{height: 0, position: 'relative', top: -20, left: 20}}>
+        <Grid item>
             <Tooltip title={'Widget settings'}>
                 <IconButton onClick={handleOpenSettings}>
                     <Settings/>
                 </IconButton>
             </Tooltip>
         </Grid>
-        <Grid item style={{position: 'relative', left: 20, top: -20}}>
+        <Grid item>
             <Tooltip title={'Delete this widget'}>
                 <IconButton onClick={() => handleRemoveItem(0)}>
                     <Cancel style={{color: 'red'}}/>
