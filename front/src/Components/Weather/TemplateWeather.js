99fc8e7f20c60d4e6b435096ce52f6be93e67b29
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-01-31 01:27:43 +0100

    feat(front/weather): weather template begin

---
+++ front/src/Components/Weather/TemplateWeather.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Grid, Paper} from "@mui/material";
+
+export default function TemplateWeather() {
+    return <Paper style={{width: '100%', height: 150}}>
+        Weather
+    </Paper>
+}
--- front/src/Components/Weather/Weather.js
+++ front/src/Components/Weather/Weather.js
@@ -1,12 +1,23 @@
 import React from "react";
-import {Grid, Typography} from "@mui/material";
+import {Button, Grid, Typography} from "@mui/material";
+import TemplateWeather from "./TemplateWeather";
 
 export default function Weather() {
+    //todo dialog new weather
+
     return <Grid container item xs={12} sx={{p: 4}}>
         <Grid item xs={12}>
             <Typography variant={'h5'} style={{fontWeight: 'bold'}}>
                 Weather
             </Typography>
+        </Grid>
+        <Grid item xs={3}>
+            <Button fullWidth>
+                Add new weather config
+            </Button>
+        </Grid>
+        <Grid item xs={3}>
+            <TemplateWeather />
         </Grid>
     </Grid>
 }
