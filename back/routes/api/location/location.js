e60df0845947c2ef8de0d1ee6019896225264419
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-02-02 13:13:35 +0100

    refactor(data/country/cities): more data and patch serach route

--- back/routes/api/location/location.js
+++ back/routes/api/location/location.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const fctToken = require('../../../tools/fctToken');
 
 const fctCountry = require("./country");
+const fctCity = require("./city");
 
 /* GET country data */
 router.get('/country', fctCountry.getDataCountry);
+
+/* GET city data by iso*/
+router.get('/cities', fctCity.getDataCityByISO);
 
 module.exports = router;
