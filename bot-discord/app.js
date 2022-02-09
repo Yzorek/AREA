6aa1beba6c1f76a9ac5a9a0894d168110bb14979
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-02-09 14:38:49 +0100

    feat(bot-discord): first command PING and HELP

--- bot-discord/app.js
+++ bot-discord/app.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { MessageEmbed } = require('discord.js');
 
 const excString = "!!";
 
@@ -10,7 +11,23 @@ client.on('ready', () => {
 client.on("message", msg => {
     if (msg.author.bot)
         return
-
+    require("./config").commands.forEach(item => {
+        try {
+            if (msg.content === `${excString}${item.name}`)
+                item.fct(msg, client);
+        } catch (e) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor('RED')
+                .setTitle('ERROR !')
+                .setAuthor(client.user.username, client.user.displayAvatarURL())
+                .setDescription('One error as pop in my systeme, sorry try again later')
+                .setFooter('If you have question, please call WhiteWolf');
+            msg.channel.send({ embeds: [exampleEmbed] });
+            console.log("----------------------------------------------------------- ERROR -----------------------------------------------------------");
+            console.log(e);
+            console.log("-----------------------------------------------------------------------------------------------------------------------------");
+        }
+    })
 })
 
 client.login('OTQwOTEyNjkyODY3MjY0NTYz.YgOTOw.f54-ym5cgCNvwSDjY1lhK4Aa8o0')
