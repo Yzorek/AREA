e3f37add9f262346f74eecfc5b047282ce607252
ibaylon <ilian.baylon@atos.net>
2022-02-15 15:34:12 +0100

    refactor(area dialog): design of area dialog in web front

--- front/src/Components/ServiceSettings/ActionReactions.js
+++ front/src/Components/ServiceSettings/ActionReactions.js
@@ -1,71 +1,77 @@
+<<<<<<< HEAD
 import {ButtonBase, Card, Grid, Switch, Typography} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import AreaDialog from "./AreaDialog";
 import {useState} from "react";
+=======
+import { Alert, ButtonBase, Card, Grid, Paper, Switch, Typography } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import AreaDialog from "./AreaDialog";
+import { useContext, useState } from "react";
+import TutorialContext from "../Tools/TutorialContext/TutorialContext";
+>>>>>>> 3fcfcd6c... refactor(area dialog): design of area dialog in web front
 
 export default function ActionsReactions() {
     const [isAddOpen, setIsAddOpen] = useState(false)
     const [areas, setAreas] = useState([])
+    let tutorialMode = useContext(TutorialContext);
 
     const handleAddClose = (value) => {
         if (Object.keys(value).length !== 0) {              // TODO pass by request
             let newArray = areas;
             newArray.push({
-                actionId: value.actionId,
-                reactionId: value.reactionId,
-                color: '#26A2E1'
+                action: value.action,
+                reaction: value.reaction,
+                color: value.action.color,
+                isActive: true
             });
             setAreas(newArray);
         }
         setIsAddOpen(false);
     }
 
+    const handleAreActivation = (item, index) => {
+        areas[index].isActive = !areas[index].isActive;
+        setAreas([...areas]);
+        console.log(areas);
+    }
+
     return (
-        <Grid container item xs={12}>
-            <Grid container item xs={12} style={{padding: 20}}>
+        <Grid container item xs={12} style={{ padding: 20 }} spacing={2}>
+            <Grid container item xs={12}>
                 <Typography variant={'h5'} style={{fontWeight: 'bold'}}>
                     ACTIONS-REACTIONS
                 </Typography>
             </Grid>
-            <Grid container item xs={12} style={{padding: 20}}>
-                {areas.map((item, index) => {
-                    return (
-                        <Card key={`${item.actionId}-${index}-cards-service`} style={{
-                            marginRight: 50,
-                            background: item.color,
-                            width: 150,
-                            height: 150,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-between',
-                            alignItems: 'center'
-                        }}>
-                            {item.actionId}
-                            <Switch>
-                            </Switch>
-                        </Card>
-                    )
-                })}
-                <Card style={{marginRight: 50, background: '#ffffff', width: 150, height: 150}}>
-                    <ButtonBase style={{
-                        width: '100%',
-                        height: '100%',
-                        paddingTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        position: 'relative'
-                    }}
-                                onClick={() => setIsAddOpen(true)}>
-                        <AddIcon style={{
-                            width: '12vw',
-                            height: '12vh',
-                            position: 'absolute',
-                            top: '50%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)'
-                        }}/>
+            {tutorialMode.isActive && <Grid container item xs={12}>
+                <Alert severity="info" style={{width: '100%'}}>Now link your actions and reactions available with your services.</Alert>
+            </Grid>}
+            <Grid container item xs={12} spacing={2}>
+                {areas.map((item, index) =>
+                    <Grid item xs={4} key={`${item.action.service}-${index}-cards-service`}>
+                        <Paper style={{height: 140, background: item.isActive ? item.color : 'gray', cursor: 'pointer'}} sx={{
+                                        transition: '0.5s',
+                                        '&:hover': {boxShadow: 12}
+                                    }} elevation={5} onClick={() => handleAreActivation(item, index)}>
+                            <Grid container item xs={12} direction={'column'} alignItems={'center'} justifyContent={'center'} sx={{p: 2}} style={{height: '100%'}}>
+                                {item.icon}
+                                <Typography color={'white'} style={{fontWeight: 'bold'}}>
+                                    {item.name}
+                                </Typography>
+                            </Grid>
+                            <Grid container item xs={12} direction={'column'} alignItems={'center'} justifyContent={'center'} sx={{p: 2}} style={{height: '100%'}}>
+                                {item.icon}
+                                <Typography color={'white'} style={{fontWeight: 'bold'}}>
+                                    {item.name}
+                                </Typography>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                )}
+                <Card style={{marginLeft: 20, marginTop: 15, background: '#ffffff', width: 150, height: 150}}>
+                    <ButtonBase style={{width: '100%', height: '100%', paddingTop: 8, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', position: 'relative'}}
+                    onClick={() => setIsAddOpen(true)}>
+                    <AddIcon style={{width: '12vw', height: '12vh', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}/>
                     </ButtonBase>
                 </Card>
                 <AreaDialog
--- front/src/Components/ServiceSettings/AreaDialog.js
+++ front/src/Components/ServiceSettings/AreaDialog.js
@@ -1,6 +1,9 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, FormControl, Grid, InputLabel, ListItemIcon, ListItemText, MenuItem, Select } from "@mui/material";
+import { Alert, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Grid, InputLabel, ListItemIcon, ListItemText, MenuItem, Select, TextField } from "@mui/material";
 import { useState } from "react";
 import { Actions, Reactions } from "./ActionsList";
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import { Cancel, Save } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
 
 export default function AreaDialog({isAddOpen, onClose}) {
     const [action, setAction] = useState('')
@@ -20,8 +23,8 @@ export default function AreaDialog({isAddOpen, onClose}) {
             return;
         }
         onClose({
-            actionId: action,
-            reactionId: reAction
+            action: action,
+            reaction: reAction
         });
         setAction('');
         setReAction('');
@@ -30,6 +33,7 @@ export default function AreaDialog({isAddOpen, onClose}) {
     }
 
     const handleActChange = (event) => {
+        console.log(event.target.value);
         setAction(event.target.value);
     }
 
@@ -38,61 +42,79 @@ export default function AreaDialog({isAddOpen, onClose}) {
     }
 
     return (
-        <Dialog onClose={handleClose} open={isAddOpen} fullWidth={true} maxWidth={'sm'}>
-            <Grid container item xs={12} justifyContent={'center'} alignItems={'center'} direction={'column'}>
-                <DialogTitle>Choose action and reaction</DialogTitle>
-                <DialogContent>
-                    <Grid container item xs={12} justifyContent={'center'} alignItems={'center'} direction={'column'}>
-                        <DialogContentText style={{paddingBottom: 10}}>
-                            You can set a maximum of 30 action-reaction.
-                        </DialogContentText>
-                        <FormControl fullWidth error={isActionNeeded ? true : false}>
-                            <InputLabel>Action</InputLabel>
-                            <Select
-                            value={action}
-                            label="Action"
-                            onChange={handleActChange}
-                            style={{width: '100%'}}>
-                                {Actions.map((element, index) => {
-                                    return (
-                                        <MenuItem value={element.id} key={`${element.id}-${index}-menuitems-action`}>
-                                            <ListItemIcon>
-                                                {element.icon}
-                                            </ListItemIcon>
-                                            <ListItemText>{element.text}</ListItemText>
-                                        </MenuItem>
-                                    )
-                                })}
-                            </Select>
-                        </FormControl>
-                        <Divider orientation="vertical" style={{height: 128, marginTop: 20, marginBottom: 20, borderRightWidth: 2}}/>
-                        <FormControl fullWidth error={isReActionNeeded ? true : false}>
-                            <InputLabel>Reaction</InputLabel>
-                            <Select
-                            value={reAction}
-                            label="ReAction"
-                            onChange={handleReActChange}
-                            style={{width: '100%'}}>
-                                {Reactions.map((element, index) => {
-                                    return (
-                                        <MenuItem value={element.id} key={`${element.id}-${index}-menuitems-reaction`}>
-                                            <ListItemIcon>
-                                                {element.icon}
-                                            </ListItemIcon>
-                                            <ListItemText>{element.text}</ListItemText>
-                                        </MenuItem>
-                                    )
-                                })}
-                            </Select>
-                        </FormControl>
+        <Dialog onClose={handleClose} open={isAddOpen} fullWidth={true} maxWidth={'md'}>
+            <DialogTitle>Choose action and reaction</DialogTitle>
+            <DialogContent>
+                <Grid container item xs={12} justifyContent={'center'} alignItems={'center'} direction={'column'}>
+                    <Grid container item xs={12}>
+                        <Alert severity="info" style={{width: '100%'}}>You can set a maximum of 30 action-reaction.</Alert>
                     </Grid>
-                </DialogContent>
-            </Grid>
-            <DialogActions>
-                <Grid container item xs={12} justifyContent={'space-between'}>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleSave}>Save</Button>
+                    <Grid container item xs={12} style={{marginTop: 7}} spacing={2} direction={'row'}>
+                        <Grid item xs={5}>
+                            <FormControl fullWidth error={isActionNeeded ? true : false}>
+                                <InputLabel>Action</InputLabel>
+                                <Select
+                                value={action}
+                                label="Action"
+                                onChange={handleActChange}
+                                renderValue={(value) => value.text}
+                                style={{width: '100%'}}>
+                                    {Actions.map((element, index) => {
+                                        return (
+                                            <MenuItem value={element} key={`${element.id}-${index}-menuitems-action`}>
+                                                <ListItemIcon>
+                                                    {element.icon}
+                                                </ListItemIcon>
+                                                <ListItemText>{element.text}</ListItemText>
+                                            </MenuItem>
+                                        )
+                                    })}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid container item xs={2} justifyContent={'center'} alignItems={'center'}>
+                            <ArrowRightAltIcon sx={{fontSize: 50}}></ArrowRightAltIcon>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <FormControl fullWidth error={isReActionNeeded ? true : false}>
+                                <InputLabel>Reaction</InputLabel>
+                                <Select
+                                value={reAction}
+                                label="ReAction"
+                                onChange={handleReActChange}
+                                renderValue={(value) => value.text}
+                                style={{width: '100%'}}>
+                                    {Reactions.map((element, index) => {
+                                        return (
+                                            <MenuItem value={element} key={`${element.id}-${index}-menuitems-reaction`}>
+                                                <ListItemIcon>
+                                                    {element.icon}
+                                                </ListItemIcon>
+                                                <ListItemText>{element.text}</ListItemText>
+                                            </MenuItem>
+                                        )
+                                    })}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid container item xs={12}>
+                            <Grid container item xs={6} justifyContent={'center'} alignItems={'center'}>
+                                <TextField></TextField>
+                            </Grid>
+                            <Grid container item xs={6} justifyContent={'center'} alignItems={'center'}>
+                                <TextField></TextField>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button startIcon={<Cancel/>} variant={'contained'} color={'secondary'} onClick={handleClose}>
+                    CANCEL
+                </Button>
+                <LoadingButton loading={false} startIcon={<Save/>} variant={'contained'} color={'primary'} onClick={handleSave}> {/*TODO loading*/}
+                    SAVE
+                </LoadingButton>
             </DialogActions>
         </Dialog>
     )
