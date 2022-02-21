bdcad2a19914e93f3821324cc2fb279f2eaebaea
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-02-21 17:40:36 +0100

    feat(back/AR): adding params management and all road for link AR

--- back/routes/actionReaction/actionReaction.js
+++ back/routes/actionReaction/actionReaction.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fctToken = require('../../tools/fctToken');
 
 const fctGetAR = require("./getAR");
+const fctLink = require("./link");
 
 router.get('/', fctToken.auth, fctGetAR.getServices, fctGetAR.getLinkService, fctGetAR.getActions, fctGetAR.getReactions, (req, res) => res.status(200).send({
     actions: res.locals.actions,
@@ -13,8 +14,12 @@ router.get('/actions', fctToken.auth, fctGetAR.getServices, fctGetAR.getLinkServ
 
 router.get('/reactions', fctToken.auth, fctGetAR.getServices, fctGetAR.getLinkService, fctGetAR.getReactions, (req, res) => res.status(200).send(res.locals.reactions))
 
-router.post('/link', fctToken.auth, )
+router.get('/link', fctToken.auth, fctLink.getLink)
 
-router.put('/link/:id', fctToken.auth, )
+router.post('/link', fctToken.auth, fctLink.newLink)
+
+router.put('/link/:id', fctToken.auth, fctLink.updateLink)
+
+router.delete('/link/:id', fctToken.auth, fctLink.deleteLink)
 
 module.exports = router;
