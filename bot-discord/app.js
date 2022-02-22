e4eb5283722e9dd94062492ef98d25150dac042c
Jules C <42450902+Jules-gitclerc@users.noreply.github.com>
2022-02-22 17:03:04 +0100

    feat(discord): notification template

--- bot-discord/app.js
+++ bot-discord/app.js
@@ -1,6 +1,6 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const { MessageEmbed } = require('discord.js');
+const {Client, Intents} = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const {MessageEmbed} = require('discord.js');
 
 const excString = "!!";
 
@@ -14,6 +14,22 @@ client.on("message", msg => {
     console.log(channel);
     if (msg.author.bot)
         return
+    require("./commands/twitch/twitch")(msg, client, {
+        id: '44777820604',
+        user_id: '50597026',
+        user_login: 'ponce',
+        user_name: 'Ponce',
+        game_id: '490100',
+        game_name: 'Lost Ark',
+        type: 'live',
+        title: 'PONCE - Oh la grosse journée MEUPORG avec Onu et RdBidet ! !lostark | !rs !boutique',
+        viewer_count: 9385,
+        started_at: '2022-02-22T09:43:08Z',
+        language: 'fr',
+        thumbnail_url: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_ponce-500x400.jpg',
+        tag_ids: [Array],
+        is_mature: true,
+})
     require("./config").commands.forEach(item => {
         try {
             if (msg.content === `${excString}${item.name}`)
@@ -25,7 +41,7 @@ client.on("message", msg => {
                 .setAuthor(client.user.username, client.user.displayAvatarURL())
                 .setDescription('One error as pop in my systeme, sorry try again later')
                 .setFooter('If you have question, please call WhiteWolf');
-            msg.channel.send({ embeds: [exampleEmbed] });
+            msg.channel.send({embeds: [exampleEmbed]});
             console.log("----------------------------------------------------------- ERROR -----------------------------------------------------------");
             console.log(e);
             console.log("-----------------------------------------------------------------------------------------------------------------------------");
