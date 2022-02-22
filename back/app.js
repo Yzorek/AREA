e87ff080499e1dc375504c755de15e7cd91cc088
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-02-22 15:24:35 +0100

    feat(back/twitch): twitch get is on live

--- back/app.js
+++ back/app.js
@@ -48,6 +48,7 @@ app.use('/AR', ARRouter);
 
 require('./bot_discord/app');
 require('./socket/socket')(io);
+require('./twitch/twitch').getStream().then(r => console.log("END !"));
 
 server.listen(8080, () => {
     console.log('listening on *:8080');
