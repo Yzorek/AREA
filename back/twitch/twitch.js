117a051551c532338843056236c590ac68c9e9a0
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-02-28 14:22:44 +0100

    feat(back/twitch): New AR overflow viewer amount

--- back/twitch/twitch.js
+++ back/twitch/twitch.js
@@ -1,7 +1,8 @@
 const fctDataBase = require("../tools/fctDBRequest");
 const TwitchApi = require("node-twitch").default;
 
-let alreadyPush = []
+let alreadyPushChannelStartNew = []
+let alreadyPushOverflow = []
 
 const twitch = new TwitchApi({
     client_id: "t7f8n8gwo0206l8n3vduq1sqkeb26y", //a mettre dans le fichier config
@@ -39,21 +40,48 @@ async function ChannelStartNewStream(data) {
 
         let result_stream = await twitch.getStreams({ channel: params_action[0].value })
         //console.log(result_stream, params_action[0].value)
-        if (result_stream.data && result_stream.data.length > 0 && !alreadyPush.find(elem => elem.id_user === data.id_user && elem.streamerName === params_action[0].value && elem.id_reactions === data.id_reactions)) {
+        if (result_stream.data && result_stream.data.length > 0 && !alreadyPushChannelStartNew.find(elem => elem.id_user === data.id_user && elem.streamerName === params_action[0].value && elem.id_reactions === data.id_reactions)) {
             if (data.id_reactions === 3) {
                 result_stream.data.forEach(item => require('../bot_discord/app').sendMessageTwitchInGuilds(params_reaction[1].value, params_reaction[0].value, item))
             } else if (data.id_reactions === 1) {
-                console.log("lmalalalalalalla")
                 result_stream.data.forEach(item => require('../tools/fctMail').sendMailTwitch(params_reaction[0].value, item))
             }
-            alreadyPush.push({id_user: data.id_user, streamerName: params_action[0].value, id_reactions: data.id_reactions})
+            alreadyPushChannelStartNew.push({id_user: data.id_user, streamerName: params_action[0].value, id_reactions: data.id_reactions})
             console.log("New stream")
         } else if (!result_stream.data || result_stream.data.length <= 0) {
-            let index = alreadyPush.findIndex(elem => elem.id_user === data.id_user && elem.streamerName === params_action[0].value && elem.id_reactions === data.id_reactions)
-            alreadyPush.splice(index, 1);
+            let index = alreadyPushChannelStartNew.findIndex(elem => elem.id_user === data.id_user && elem.streamerName === params_action[0].value && elem.id_reactions === data.id_reactions)
+            alreadyPushChannelStartNew.splice(index, 1);
             console.log("Stream disconnect")
         } else {
             console.log("Already Emit")
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function ChannelStartOverflow(data) {
+    try {
+        let params_action = JSON.parse(data.params_action)
+        let params_reaction = JSON.parse(data.params_reaction)
+
+        let result_stream = await twitch.getStreams({ channel: params_action[0].value })
+        //console.log(result_stream, params_action[0].value)
+        if (result_stream.data && result_stream.data.length > 0 && parseInt(params_action[1].value) >= result_stream.viewer_count && !alreadyPushOverflow.find(elem => elem.id_user === data.id_user && elem.streamerName === params_action[0].value && elem.id_reactions === data.id_reactions)) {
+            if (data.id_reactions === 3) {
+                result_stream.data.forEach(item => require('../bot_discord/app').sendMessageTwitchInGuilds(params_reaction[1].value, params_reaction[0].value, item))
+            } else if (data.id_reactions === 1) {
+                result_stream.data.forEach(item => require('../tools/fctMail').sendMailTwitch(params_reaction[0].value, item))
+            }
+            alreadyPushOverflow.push({id_user: data.id_user, streamerName: params_action[0].value, id_reactions: data.id_reactions})
+            console.log("New stream overflow")
+        } else if (!result_stream.data || result_stream.data.length <= 0 || parseInt(params_action[1].value) < result_stream.viewer_count) {
+            let index = alreadyPushOverflow.findIndex(elem => elem.id_user === data.id_user && elem.streamerName === params_action[0].value && elem.id_reactions === data.id_reactions)
+            alreadyPushOverflow.splice(index, 1);
+            console.log("Stream disconnect overflow or below")
+        } else {
+            console.log("Already Emit overflow or sup")
         }
 
     } catch (err) {
@@ -67,9 +95,12 @@ async function reloadStreamsManagement() {
 
         linkForTwitch.forEach(item => {
             console.log(item);
-            if (item.id_actions === 9) {
+            if (item.id_actions === 9 || item.id_actions === 4) {
                 console.log("==== Result Stream ====")
                 ChannelStartNewStream(item)
+            } else if (item.id_actions === 8) {
+                console.log("==== Amout view ====")
+                ChannelStartOverflow(item)
             }
         })
     } catch (err) {
