1c7eafa0778f45839f9cd0bee136062033965d69
Jules C <42450902+Jules-gitclerc@users.noreply.github.com>
2022-02-28 15:35:08 +0100

    feat(telegram): bot function to response to user and group with array to stock it

--- back/app.js
+++ back/app.js
@@ -48,8 +48,8 @@ app.use('/download', downloadRouter);
 app.use('/AR', ARRouter);
 //app.use('/api/twitch', apiTwitchRouter);
 
-app.get('/telegram', () => {
-    sendMessageTwitchInTelegram({
+app.get('/telegramUser', () => {
+    sendMessageTwitchInTelegramToUser({
         id: '44777820604',
         user_id: '50597026',
         user_login: 'ponce',
@@ -64,12 +64,31 @@ app.get('/telegram', () => {
         thumbnail_url: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_ponce-500x400.jpg',
         tag_ids: [Array],
         is_mature: true,
-    })
+    }, "Surfy971")
+})
+
+app.get('/telegramGroup', () => {
+    sendMessageTwitchInTelegramToGroup({
+        id: '44777820604',
+        user_id: '50597026',
+        user_login: 'ponce',
+        user_name: 'Ponce',
+        game_id: '490100',
+        game_name: 'Lost Ark',
+        type: 'live',
+        title: 'PONCE - Oh la grosse journée MEUPORG avec Onu et RdBidet ! !lostark | !rs !boutique',
+        viewer_count: 9385,
+        started_at: '2022-02-22T09:43:08Z',
+        language: 'fr',
+        thumbnail_url: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_ponce-500x400.jpg',
+        tag_ids: [Array],
+        is_mature: true,
+    }, "Zone AREA")
 })
 
 require('./bot_discord/app');
 require('./bot_telegram/app')
-const {sendMessageTwitchInTelegram} = require("./bot_telegram/app");
+const {sendMessageTwitchInTelegram, sendMessageTwitchInTelegramToUser, sendMessageTwitchInTelegramToGroup} = require("./bot_telegram/app");
 require('./socket/socket')(io);
 //require('./twitch/twitch').getStream().then(r => console.log("END !"));
 
--- back/bot_telegram/app.js
+++ back/bot_telegram/app.js
@@ -1,22 +1,41 @@
 const axios = require('axios')
+const { Telegraf } = require('telegraf')
 
 const token = '5160965468:AAHAcyYNrKYWrRCR_9eOfYl94Z6DIWSk7KM';
 
 const chatIDGroup = -730273977
 const chatIDUser = 922550661
 
+let myUser = []
+let myGroup = []
+
+const bot = new Telegraf(token)
+bot.command('start', (ctx) => {
+    if (ctx.chat.username)
+        myUser.push(ctx.chat)
+    console.log(ctx.chat)
+    if (ctx.chat.title)
+        myGroup.push(ctx.chat)
+    ctx.reply('Initialize')
+})
+bot.launch()
+
 const telegramUrl = "https://api.telegram.org/bot" + token;
 //axios.post(telegramUrl + "/sendMessage?chat_id=" + chatIDUser + "&text=" + "test is on stream")
 
-function sendMessageTwitchInTelegramGroup(data) {
-    axios.post(telegramUrl + "/sendMessage?chat_id=" + chatIDGroup + "&text=" + data.user_name + "is on stream")
+function sendMessageTwitchInTelegramToUser(data, user_name) {
+    let target = myUser.find(item => item.username.toLowerCase() === user_name.toLowerCase())
+    if (target)
+        axios.post(telegramUrl + "/sendMessage?chat_id=" + target.id + "&text=" + data.user_name + " is on stream")
 }
 
-function sendMessageTwitchInTelegramUser(chatID, data) {
-    axios.post(telegramUrl + "/sendMessage?chat_id=" + chatID + "&text=" + data.user_name + "is on stream")
+function sendMessageTwitchInTelegramToGroup(data, group_name) {
+    let target = myGroup.find(item => item.title.toLowerCase() === group_name.toLowerCase())
+    if (target)
+        axios.post(telegramUrl + "/sendMessage?chat_id=" + target.id + "&text=" + data.user_name + " is on stream")
 }
 
 module.exports = {
-    sendMessageTwitchInTelegramGroup,
-    sendMessageTwitchInTelegramUser,
+    sendMessageTwitchInTelegramToUser,
+    sendMessageTwitchInTelegramToGroup,
 }
