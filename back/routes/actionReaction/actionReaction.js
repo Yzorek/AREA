5786bc4b655aa75c8fd17761870c92d5468be7dd
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-03-05 16:44:52 +0100

    feat(routes): new road for ilian link and service

--- back/routes/actionReaction/actionReaction.js
+++ back/routes/actionReaction/actionReaction.js
@@ -4,6 +4,7 @@ const fctToken = require('../../tools/fctToken');
 
 const fctGetAR = require("./getAR");
 const fctLink = require("./link");
+const fctAll = require("../services/all");
 
 router.get('/', fctToken.auth, fctGetAR.getServices, fctGetAR.getLinkService, fctGetAR.getActions, fctGetAR.getReactions, (req, res) => res.status(200).send({
     actions: res.locals.actions,
@@ -18,7 +19,12 @@ router.get('/actions', fctToken.auth, fctGetAR.getServices, fctGetAR.getLinkServ
 
 router.get('/reactions', fctToken.auth, fctGetAR.getServices, fctGetAR.getLinkService, fctGetAR.getReactions, (req, res) => res.status(200).send(res.locals.reactions))
 
-router.get('/link', fctToken.auth, fctLink.getLink)
+router.get('/link', fctToken.auth, fctLink.getLink, (req, res) => res.status(200).send(res.locals.target))
+
+router.get('/ilian', fctToken.auth, fctLink.getLink,  fctGetAR.getServices, fctGetAR.getLinkService, (req, res) => res.status(200).send({
+    link: res.locals.target,
+    services: res.locals.services,
+}))
 
 router.get('/service/link' /*?idService=*/, fctToken.auth, fctLink.getReactions, fctLink.getActions, fctLink.getLinkByService)
 
@@ -27,5 +33,6 @@ router.post('/link', fctToken.auth, fctLink.newLink)
 router.put('/link/:id', fctToken.auth, fctLink.updateLink)
 
 router.delete('/link/:id', fctToken.auth, fctLink.deleteLink)
+
 
 module.exports = router;
