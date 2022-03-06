6839866eb5dd0ff8821fe250c3d9900cf30e64da
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-03-06 19:23:21 +0100

    feat(back/discord): new commands

--- back/bot_discord/config.js
+++ back/bot_discord/config.js
@@ -1,7 +1,7 @@
 const commands = [
     {name: 'ping', fct: require("./commands/ping/ping"), description: 'Command for testing if bot is active.'},
     {name: 'help', fct: require("./commands/help/help"), description: 'Command for display all commands.'},
-    {name: 'settings', fct: require("./commands/settings/settings"), description: 'Command for set your account.'},
+    {name: 'start', fct: require("./commands/settings/settings"), description: 'Command for set your account.'},
 ]
 
 module.exports = {
